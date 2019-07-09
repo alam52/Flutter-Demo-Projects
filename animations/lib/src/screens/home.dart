3386083378f624e7d0cb7faf7ab222a7ead09bc0
Zaid Alam <zalam307@gmail.com>
2019-07-09 19:19:36 +0530

    Added animation

--- animations/lib/src/screens/home.dart
+++ animations/lib/src/screens/home.dart
@@ -6,17 +6,52 @@ class Home extends StatefulWidget{
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home>{
+class HomeState extends State<Home> with TickerProviderStateMixin{
+
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState(){
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      )
+    );
+  }
+
+  onTap(){
+    catController.forward();
+  }
+
   Widget build(context){
       return Scaffold(
         appBar: AppBar(
           title: Text('Animations!'),
         ),
-        body: buildAnimation(),
+        body: GestureDetector(
+          child: buildAnimation(),
+          onTap: onTap,
+        )
       );
   }
 
   Widget buildAnimation(){
-    return Cat();
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child){
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   } 
 }
