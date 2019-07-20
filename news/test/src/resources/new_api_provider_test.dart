e85fa98aa0898929091e5976ee81bb3f520e940e
Zaid Alam <zalam307@gmail.com>
2019-07-20 23:48:11 +0530

    Test file updated

--- news/test/src/resources/new_api_provider_test.dart
+++ news/test/src/resources/new_api_provider_test.dart
@@ -1,14 +1,30 @@
-  import 'package:news/src/resources/news_api_provider.dart';
+  import 'package:flutter_test/flutter_test.dart' as prefix0;
+import 'package:news/src/resources/news_api_provider.dart';
   import 'dart:convert';
   import 'package:test_api/test_api.dart';
   import 'package:http/http.dart';
   import 'package:http/testing.dart';
 
   void main(){
-    test('FetchTopIds returns a list of Ids',(){
-      //setup of test case
-      final sum = 1 + 3;
-      //expectation
-      expect(sum,4);
+    test('FetchTopIds returns a list of Ids',() async {
+      final newsApi = NewsApiProvider();
+      newsApi.client = MockClient((request) async {
+        return Response(json.encode([1,2,3,4]), 200);
+      });
+
+      final ids = await newsApi.fetchTopIds();
+      expect(ids, [1,2,3,4]);
     });
+
+    test('FetchItem returns ItemModel',() async {
+      final newsApi = NewsApiProvider();
+      newsApi.client = MockClient((request) async{
+        final jsonMap = {'id':123};
+        return Response(json.encode(jsonMap),200);
+      });
+      
+      final item = await newsApi.fetchItem(999);
+      expect(item.id,123);
+    });
+
   }
